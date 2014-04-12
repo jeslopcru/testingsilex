1b403a68c87c7c6e2be7e762c30df46b81d36263
jeslopcru <jeslopcru@gmail.com>
2014-04-12 23:08:33 +0200

    add log in app

--- README.md
+++ README.md
@@ -2,3 +2,8 @@ testingsilex
 ============
 
 Testing Silex framework using Phpunit
+
+#Step 1
+Añadir log a la aplicación
+
+*No hay que olvidar dar permisos al archivo de log
--- app/bootstrap.php
+++ app/bootstrap.php
@@ -2,10 +2,20 @@
 
 require_once(__DIR__ . '/../vendor/autoload.php');
 
+use Silex\Provider\MonologServiceProvider;
+
 $app = new Silex\Application;
 
+$app->register(new MonologServiceProvider(), array(
+    'monolog.logfile' => __DIR__ . '/../logs/dev.log'
+));
+
 $app
-    ->match('/', function () {return 'Running';})
-    ->method('GET');
+    ->match('/', function () use ($app) {
+        $app['monolog']->addInfo('Logging example in the status route');
+
+        return 'Up and running';
+    })
+    ->method('GET|POST');
 
 return $app;
