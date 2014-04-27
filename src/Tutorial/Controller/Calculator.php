f16fc00273d3cca3e3e1683d027a1cfaac4e3b5d
jeslopcru <jeslopcru@gmail.com>
2014-04-27 19:12:45 +0200

    Añadir tests

--- src/Tutorial/Controller/Calculator.php
+++ src/Tutorial/Controller/Calculator.php
@@ -1,10 +1,4 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: mardurnar
- * Date: 27/04/14
- * Time: 19:02
- */
 
 namespace Tutorial\Controller;
 
---
+++ tests/Controller/CalculatorTest.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mardurnar
+ * Date: 27/04/14
+ * Time: 19:12
+ */
+
+namespace Controller;
+
+
+class CalculatorTest extends \PHPUnit_Framework_TestCase {
+
+}
+ 
